52c6cd9e1b3b2e8717f431ca57a5678948e7bc20
Shreesha Srinath <shreesha.srinath@esperantotech.com>
2018-04-11 15:40:46 -0700

    changes to get SimSerial working with BOOM
    NOTE: BOOM configs must be changed to use the BoomROM image in project-template!
    
      https://github.com/ucb-bar/project-template/blob/master/bootrom/bootrom.rv64.img

---
+++ src/main/scala/serial/Top.scala
@@ -0,0 +1,45 @@
+package serial
+
+import chisel3._
+import freechips.rocketchip.subsystem._
+import freechips.rocketchip.config.Parameters
+import freechips.rocketchip.devices.tilelink._
+import freechips.rocketchip.util.DontTouch
+import testchipip._
+import boom.system.{BoomSubsystem, BoomSubsystemModule}
+
+class ExampleTop(implicit p: Parameters) extends BoomSubsystem
+    with HasAsyncExtInterrupts
+    with HasMasterAXI4MemPort
+    with HasPeripheryBootROM
+    with HasSystemErrorSlave
+    with HasNoDebug
+    with HasPeripherySerial {
+  override lazy val module = new ExampleTopModule(this)
+}
+
+class ExampleTopModule[+L <: ExampleTop](l: L) extends BoomSubsystemModule(l)
+    with HasRTCModuleImp
+    with HasExtInterruptsModuleImp
+    with HasMasterAXI4MemPortModuleImp
+    with HasPeripheryBootROMModuleImp
+    with HasNoDebugModuleImp
+    with HasPeripherySerialModuleImp
+    with DontTouch
+
+class ExampleTopWithPWM(implicit p: Parameters) extends ExampleTop
+    with HasPeripheryPWM {
+  override lazy val module = new ExampleTopWithPWMModule(this)
+}
+
+class ExampleTopWithPWMModule(l: ExampleTopWithPWM)
+  extends ExampleTopModule(l) with HasPeripheryPWMModuleImp
+
+class ExampleTopWithBlockDevice(implicit p: Parameters) extends ExampleTop
+    with HasPeripheryBlockDevice {
+  override lazy val module = new ExampleTopWithBlockDeviceModule(this)
+}
+
+class ExampleTopWithBlockDeviceModule(l: ExampleTopWithBlockDevice)
+  extends ExampleTopModule(l)
+  with HasPeripheryBlockDeviceModuleImp
